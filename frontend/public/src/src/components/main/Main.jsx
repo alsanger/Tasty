fbe916f01d73bffd2bf3742b828775e01b9b78ff
alsanger <san-mail@ukr.net>
2025-02-25 21:18:00 +0200

    Errors...

--- frontend/public/src/src/components/main/Main.jsx
+++ frontend/public/src/src/components/main/Main.jsx
@@ -5,19 +5,17 @@ import ImageUploader from "./ImageUploader.jsx";
 
 function Main() {
     return (
-        <div className={container}>
-            <div className="main">
-                <CountryButtonsNavigation/>
-                <p>Это основное содержимое твоего приложения.</p>
-                <h2>Загрузка аватара</h2>
-                <ImageUploader type="avatar" id={10}/>
+        <div className="main">
+            <CountryButtonsNavigation />
+            <p>Это основное содержимое твоего приложения.</p>
+            <h2>Загрузка аватара</h2>
+            <ImageUploader type="avatar" id={10} />
 
-                {/* <h2>Загрузка изображения рецепта</h2>
+            {/* <h2>Загрузка изображения рецепта</h2>
             <ImageUploader type="recipe" id={1} /> */}
 
-                {/* <h2>Загрузка изображения шага рецепта</h2>
+            {/* <h2>Загрузка изображения шага рецепта</h2>
             <ImageUploader type="recipe" id={1} recipeStepId={2} /> */}
-            </div>
         </div>
     );
 }
